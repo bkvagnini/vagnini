2a8b9e4e164d2fb75703df6fce47d99153510f0f
Brian K. Vagnini <brian@vagnini.net>
2018-09-27 21:13:03 -0400

    Cleanup old files.

--- test/uprunningangular/js/controllers.js
+++
@@ -1,8 +0,0 @@
-var myNameSpace = angular.module('myApp', []);
-
-myNameSpace.controller('MyController',function MyController ($scope, $http) {
-   $http.get('js/data.json').success(function(data) {
-     
-      $scope.artists = data;
-    }); // ends function 
-    }); //ends myNameSpace.controller
